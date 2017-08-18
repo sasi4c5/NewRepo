6f884ff7455e3231cc5db9b6d116f12fa7caf4cc
Shashikanth Suram <sasikanthsuram@outlook.com>
2017-08-18 17:57:21 -0500

    n

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Services.Actions;
+using Services.Model;
+
+namespace WebApi.Controllers
+{
+    public class AuthorController : ApiController
+    {
+        private AuthorAction authActions;
+
+        public IEnumerable<Services.Model.AuthorModel> Get()
+        {
+            authActions = new AuthorAction();
+            return authActions.GetAllAuthors();
+        }
+
+        public AuthorModel Get(string id)
+        {
+            authActions = new AuthorAction();
+            return authActions.GetByName(id);
+        }
+        public void Post([FromBody] AuthorModel val)
+        {
+            authActions = new AuthorAction();
+            authActions.AddAuthor(val);
+        }
+        public void Put(int id, [FromBody] AuthorModel LcAu)
+        {
+
+            authActions = new AuthorAction();
+            authActions.EditAuthor(id, LcAu);
+
+        }
+        public void Delete(int id)
+        {
+            authActions = new AuthorAction();
+            authActions.DeleteAuthor(id);
+        }
+    }
+    
+
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Services.Actions;
+using Services.Model;
+
+namespace WebApi.Controllers
+{
+    public class BookController : ApiController
+    {
+        private BookAction bookActions;
+        public IEnumerable<BookModel> Get()
+        {
+            bookActions = new BookAction();
+            return bookActions.GetAllBooks();
+
+        }
+
+        public BookModel Get(string id)
+        {
+            bookActions = new BookAction();
+            return bookActions.GetByBook(id);
+        }
+    }
+}
